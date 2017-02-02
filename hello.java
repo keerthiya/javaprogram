18957971fa9742f5dc9e56f8c2ba93c58d8a0af4
keerthiya <keerthiyamathavan@yahoo.com>
2017-02-02 14:58:03 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class hello
+{
+ public static void main(String[] args)
+  {
+    int i;
+    for(i=0;i<5;i++)
+     {
+      System.out.println("Hello world");
+     }
+   }
+   }
+    
